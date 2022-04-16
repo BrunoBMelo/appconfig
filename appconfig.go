a63c9693c1dcef159f0f84df66de174e2254d34f
Bruno Melo <bruno.b.melo@live.com>
2022-04-16 12:44:05 -0300

    fix appconfig

--- appconfig.go
+++ appconfig.go
@@ -2,7 +2,6 @@ package appconfig
 
 import (
 	"context"
-	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -21,54 +20,39 @@ type Config struct {
 
 func LoadConfig() Config {
 
-	configApp := Config{}
 	godotenv.Load()
-
+	configApp := Config{}
 	env := os.Getenv("ENVIRONMENT")
+	configApp.PortApp = os.Getenv("PORT")
+	configApp.localstackRegion = os.Getenv("LOCALSTACK_AWS_REGION")
+	configApp.awsPartitionId = os.Getenv("LOCALSTACK_PARTITION_ID")
+	configApp.localstackUrl = os.Getenv("LOCALSTACK_URL")
+	configApp.PortApp = os.Getenv("PORT")
+
 	if env == "dev" {
 
-		if err := godotenv.Load(".env"); err != nil {
-			log.Fatalf("Error loading .env file for development environment")
-		}
-
 		configApp.isDevelopment = true
-		configApp.PortApp = os.Getenv("PORT")
-		configApp.localstackRegion = os.Getenv("LOCALSTACK_AWS_REGION")
-		configApp.awsPartitionId = os.Getenv("LOCALSTACK_PARTITION_ID")
-		configApp.localstackUrl = os.Getenv("LOCALSTACK_URL")
-
-		cfg, err := config.LoadDefaultConfig(context.TODO(),
+		if cfg, err := config.LoadDefaultConfig(context.TODO(),
 			config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 				return aws.Endpoint{
 					PartitionID:   configApp.awsPartitionId,
 					URL:           configApp.localstackUrl,
 					SigningRegion: configApp.AwsConfig.Region,
 				}, nil
-			})))
-
-		if err != nil {
+			}))); err != nil {
 			panic(err)
+		} else {
+			configApp.AwsConfig = &cfg
 		}
-
-		configApp.AwsConfig = &cfg
 
 	} else {
 
-		cfg, err := config.LoadDefaultConfig(context.Background())
-		if err != nil {
-			panic(err)
-		}
-
-		port := os.Getenv("PORT")
-		if port == "" {
-			panic("the port number cannot be empty")
-		}
-
-		configApp.PortApp = port
-		configApp.AwsConfig = &cfg
-		configApp.AwsConfig = &aws.Config{}
 		configApp.isDevelopment = false
-
+		if cfg, err := config.LoadDefaultConfig(context.Background()); err != nil {
+			panic(err)
+		} else {
+			configApp.AwsConfig = &cfg
+		}
 	}
 
 	return configApp
